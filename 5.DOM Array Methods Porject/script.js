480230a92e44cd0d58b4d645c338d56f3c7de6a8
JaeWon Baek <bjw1622@gmail.com>
2022-09-01 11:31:05 +0900

    Add DOM Array Method Folder

---
+++ 5.DOM Array Methods Porject/script.js
@@ -0,0 +1,11 @@
+const addUserBtn = document.querySelector('#add-user');
+
+addUserBtn.addEventListener('click',()=>{
+    fetch('https://randomuser.me/api/',{
+        method:"get"
+    })
+    .then((response)=>response.json())
+    .then((data)=>console.log(data));
+});
+
+
