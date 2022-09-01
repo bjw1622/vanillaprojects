66fa164a87a2495eafffa1bf3b48a533071c7bda
JaeWon Baek <bjw1622@gmail.com>
2022-09-01 11:52:12 +0900

    add userBtn click event

--- 5.DOM Array Methods Porject/script.js
+++ 5.DOM Array Methods Porject/script.js
@@ -1,11 +1,15 @@
 const addUserBtn = document.querySelector('#add-user');
+const main = document.querySelector('#main');
 
 addUserBtn.addEventListener('click',()=>{
     fetch('https://randomuser.me/api/',{
         method:"get"
     })
     .then((response)=>response.json())
-    .then((data)=>console.log(data));
+    .then((data)=>{
+        main.innerHTML += `<div>
+        <h3><strong>${data.results[0].name.first +" "+ data.results[0].name.last}</strong> <span class="wealth">$${(Math.floor(Math.random() * 1000000)).toFixed(2)}</span></h3>
+        </div>`});
 });
 
 
